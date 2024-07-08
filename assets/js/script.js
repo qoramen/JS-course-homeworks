51d3bb590240fb515f89b2ee262e2dfc212c2dca
qoramen <tojiboyevmurodbbek@gmail.com>
2024-07-08 19:15:50 +0500

    last task commented

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,29 +8,29 @@
 // }
 
 // first('Murodbek', function() {
-//     second('Tojiboyev')
+//     second('Tojiboyev');
 // })
 
-const toggleBtnChange = document.getElementById('toggleBtnChange');
-const toggleBtnAdd = document.getElementById('toggleBtnAdd');
-const toggleBtnRemove = document.getElementById('toggleBtnRemove');
-const title = document.getElementById('title');
-const paragraph = document.getElementById('para')
+// const toggleBtnChange = document.getElementById('toggleBtnChange');
+// const toggleBtnAdd = document.getElementById('toggleBtnAdd');
+// const toggleBtnRemove = document.getElementById('toggleBtnRemove');
+// const title = document.getElementById('title');
+// const paragraph = document.getElementById('para')
 
-toggleBtnChange.addEventListener('click', () => {
-    title.innerHTML = 'New Title text';
-    paragraph.innerHTML = 'New paragraph text';
-});
+// toggleBtnChange.addEventListener('click', () => {
+//     title.innerHTML = 'New Title text';
+//     paragraph.innerHTML = 'New paragraph text';
+// });
 
-const newPara = document.createElement('p');
-const gap = document.createTextNode('New paragraph')
-const text = document.getElementById('text')
+// const newPara = document.createElement('p');
+// const gap = document.createTextNode('New paragraph')
+// const text = document.getElementById('text')
 
-toggleBtnAdd.addEventListener('click', () => {
-    newPara.appendChild(gap)
-    text.appendChild(newPara)
-})
+// toggleBtnAdd.addEventListener('click', () => {
+//     newPara.appendChild(gap)
+//     text.appendChild(newPara)
+// })
 
-toggleBtnRemove.addEventListener('click', () => {
-    newPara.remove();
-})
+// toggleBtnRemove.addEventListener('click', () => {
+//     newPara.remove();
+// })
